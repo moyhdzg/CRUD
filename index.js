7631129f60e548f0a2db9b737e15fc341fc45148
Moises Hdz <moy.hdz.g@gmail.com>
2022-11-30 21:57:28 -0800

    Create and populate complete

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-
-function add(){
-    let id = generateID(5);
-    console.log(id);
-    let tName =  document.getElementById("taskName").value;
-    console.log(tName);
-    let tDescription = document.getElementById("taskDesc").value;
-    console.log(tDescription);
-    const currentTask1 = {id,tName,tDescription}
-    const currentTask2 = [id,tName,tDescription]
-    console.log(currentTask1);
-    console.log(currentTask2)
-    clean();
-}
- 
-
-function clean () {
-    document.getElementById("taskName").value = "";
-    document.getElementById("taskDesc").value = "";
-}
+const tasks = new Storage('tasks', taskStorage.fromObject);
 
 function generateID(length) {
     let result = ''
@@ -26,4 +7,37 @@ function generateID(length) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
+}
+
+function clean () {
+    document.getElementById("taskName").value = "";
+    document.getElementById("taskDesc").value = "";
+}
+
+
+function add(){
+    let id = generateID(5);
+    let tName =  document.getElementById("taskName").value;
+    let tDescription = document.getElementById("taskDesc").value;
+    const tCurrent = new taskStorage(id,tName,tDescription) 
+    tasks.createItem(tCurrent)
+    populateTable(tasks.readItems());
+    clean();
+}
+
+function populateTable(tasks) {
+    const tbody = document.getElementById('tableContent');
+    tbody.innerHTML = '';
+    tasks.forEach((toDo) => tbody.appendChild(createTr(toDo)));
+}
+
+function createTr(toDo) {
+    const tdHTML = `
+    <td>${toDo.getId()}</td>
+    <td>${toDo.getTask()}</td>
+    <td>${toDo.getDesc()}</td>
+    `;
+    const tr = document.createElement('tr');
+    tr.innerHTML = tdHTML;
+    return tr;
 }
--- storage.js
+++ storage.js
@@ -1,8 +1,31 @@
 class Storage {
-    constructor(item, newTask) {
-    this.item = item
-    this.name = name
-    this.description = descritpion
+    constructor(key, taskComp) {
+    this.key = key;
+    this.taskComp = taskComp;
+    this.createStorage();
     }
     
+    createStorage(){
+        if (!localStorage.getItem(this.key)) {
+            localStorage.setItem(this.key, JSON.stringify([]));
+        }
+    }
+    parseStorage() {
+        return JSON.parse(localStorage.getItem(this.key)).map((i) =>
+        this.taskComp(i),
+        );
+    }
+    saveStorage(storage) {
+        localStorage.setItem(this.key, JSON.stringify(storage));
+    }
+    // CRUD
+    createItem(item) {
+        const current = this.parseStorage();
+        current.push(item);
+        this.saveStorage(current);
+    }
+    readItems() {
+        return this.parseStorage();
+      }
+
 } 
