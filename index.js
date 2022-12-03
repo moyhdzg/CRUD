08c11abd1b8bd4cddae9cdf5cb96f49bc8e5e251
Moises Hdz <moy.hdz.g@gmail.com>
2022-12-03 15:53:53 -0800

    CRUD completo

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function generateID(length) {
 }
 
 function clean () {
+    document.getElementById("idTask").value = ""
     document.getElementById("taskName").value = "";
     document.getElementById("taskDesc").value = "";
 }
@@ -19,10 +20,15 @@ function add(){
     let id = generateID(5);
     let tName =  document.getElementById("taskName").value;
     let tDescription = document.getElementById("taskDesc").value;
-    const tCurrent = new taskStorage(id,tName,tDescription) 
-    tasks.createItem(tCurrent)
-    populateTable(tasks.readItems());
-    clean();
+    if (tName =='' || tDescription =='' ) {
+        alert('Asegura llenar todos los campos')
+    } 
+    else {
+        const tCurrent = new taskStorage(id,tName,tDescription) 
+        tasks.createItem(tCurrent)
+        populateTable(tasks.readItems());
+        clean();
+    }
 }
 
 function populateTable(tasks) {
@@ -36,8 +42,58 @@ function createTr(toDo) {
     <td>${toDo.getId()}</td>
     <td>${toDo.getTask()}</td>
     <td>${toDo.getDesc()}</td>
-    `;
+    <td class="addEdit">
+        <button id="button-edit" onclick='edit(this)'>Edit</button>
+        <button id="button-delete" onclick='deplete(this)'>Delete</button>
+    </td>`;
     const tr = document.createElement('tr');
     tr.innerHTML = tdHTML;
     return tr;
 }
+
+function edit(event) {
+    const { id, task, desc } = getDataTr(event);
+    fillInputs(id, task, desc);
+    }
+
+function fillInputs(id, task, desc) {
+    document.querySelector('input[id="idTask"]').value = id;
+    document.querySelector('input[id="taskName"]').value = task;
+    document.querySelector('input[id="taskDesc"]').value = desc;
+}
+
+function update(event) {
+    const { id, task, desc } = getInputData(event);
+    tasks.updateItem(id, (taskComp) => new taskStorage(taskComp.id, task, desc));
+    clean();
+    populateTable(tasks.readItems());
+    }
+
+function getInputData() {
+    const id = document.getElementById('idTask').value
+    const task = document.getElementById('taskName').value
+    const desc =  document.getElementById('taskDesc').value
+    return { 
+        'id' : id,
+        'task' : task,
+        'desc': desc,
+    };
+}
+
+function getDataTr(icon) {
+    const tr = icon.closest('tr');
+    const [id, task, desc] = Array.from(tr.children)
+        .slice(0, -1)
+        .map((input) => input.innerText);
+    return { id, task, desc };  
+}
+
+function deplete(event) {
+    const { id } = getDataTr(event);
+    tasks.deleteItem(id);
+    populateTable(tasks.readItems());
+}
+
+window.onload = () => {
+    populateTable(tasks.readItems());
+  };
--- storage.js
+++ storage.js
@@ -18,6 +18,16 @@ class Storage {
     saveStorage(storage) {
         localStorage.setItem(this.key, JSON.stringify(storage));
     }
+    getItemIfExists(id, current) {
+        const index = current.findIndex((item) => item.id === id);
+        if (index !== -1) {
+        return [current[index], index];
+        }
+        return null;
+    }
+    
+
+
     // CRUD
     createItem(item) {
         const current = this.parseStorage();
@@ -26,6 +36,23 @@ class Storage {
     }
     readItems() {
         return this.parseStorage();
-      }
-
+    }
+    updateItem(id, updateFunc) {
+        const current = this.parseStorage();
+        const item = this.getItemIfExists(id, current);
+        if (!!item) {
+            const [currentItem, index] = item;
+            current[index] = updateFunc(currentItem);
+            this.saveStorage(current);
+        }
+    }
+    deleteItem(id) {
+        const current = this.parseStorage();
+        const item = this.getItemIfExists(id, current);
+        if (!!item) {
+            const [_, index] = item;
+            current.splice(index, 1);
+            this.saveStorage(current);
+        }
+    }
 } 
